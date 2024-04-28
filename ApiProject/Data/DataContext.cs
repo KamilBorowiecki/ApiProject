d682c5935d24251becbd26cdb1b5fcb0fe4205dd
KamilBorowiecki <kamil.borowiecki@op.pl>
2024-04-28 12:34:41 +0200

    commit

---
+++ ApiProject/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProject.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+
+        
+    }
+}
---
+++ ApiProject/Seed.cs
@@ -0,0 +1,17 @@
+﻿using ApiProject.Data;
+
+namespace ApiProject
+{
+    public class Seed
+    {
+        private readonly DataContext dataContext;
+        public Seed(DataContext context)
+        {
+            this.dataContext = context;
+        }
+        public void SeedDataContext()
+        {
+
+        }
+    }
+}
